1a65fef441bb01c3dfcad9bdcda3a1bc63676804
chriszhou0916 <12502570+chriszhou0916@users.noreply.github.com>
2019-11-18 15:08:47 -0500

    Temporarily rolled back instance norm to batch norm for generator and di

--- trainer/models/networks.py
+++ trainer/models/networks.py
@@ -39,19 +39,19 @@ def conv_w_reflection(input_tensor,
   p = kernel_size // 2
   x = ReflectionPadding2D(padding=(p, p))(input_tensor)
   x = tf.keras.layers.Conv2D(filters, kernel_size, strides=stride, use_bias=True)(x)
-  x = normalization(x, method='instance')
+  x = normalization(x, method='batch')
   x = tf.keras.layers.Activation(tf.nn.relu)(x)
   return x
 
 def conv_block(input_tensor, filters):
   x = ReflectionPadding2D(padding=(1, 1))(input_tensor)
   x = tf.keras.layers.Conv2D(filters, kernel_size=3, strides=(1, 1), use_bias=True)(x)
-  x = normalization(x, method='instance')
+  x = normalization(x, method='batch')
   x = tf.keras.layers.Activation(tf.nn.relu)(x)
 
   x = ReflectionPadding2D(padding=(1, 1))(x)
   x = tf.keras.layers.Conv2D(filters, kernel_size=3, strides=(1, 1), use_bias=True)(x)
-  x = normalization(x, method='instance')
+  x = normalization(x, method='batch')
   return x
 
 def residual_block(input_tensor, filters):
@@ -61,7 +61,7 @@ def residual_block(input_tensor, filters):
 
 def upsample_conv(input_tensor, kernel_size, filters, stride):
   x = tf.keras.layers.Conv2DTranspose(filters, kernel_size, strides=stride, padding='same', use_bias=True)(input_tensor)
-  x = normalization(x, method='instance')
+  x = normalization(x, method='batch')
   x = tf.keras.layers.Activation(tf.nn.relu)(x)
   return x
 
@@ -144,21 +144,23 @@ def dis_downsample(input_tensor,
                kernel_size,
                filters,
                stride, norm=None):
+  initializer = tf.random_normal_initializer(0., 0.02)
   p = kernel_size // 2
   x = ReflectionPadding2D(padding=(p, p))(input_tensor)
-  x = tf.keras.layers.Conv2D(filters, kernel_size, strides=stride)(x)
+  x = tf.keras.layers.Conv2D(filters, kernel_size, strides=stride, kernel_initializer=initializer)(x)
   if norm is not None:
     x = normalization(x, method=norm)
   x = tf.keras.layers.LeakyReLU(alpha=0.2)(x)
   return x
 
 def create_discriminator(shape=(256, 256, 3)):
+    initializer = tf.random_normal_initializer(0., 0.02)
     inputs = tf.keras.layers.Input(shape=shape)
     x = dis_downsample(inputs, 4, 64, 2, norm=None)
-    x = dis_downsample(x, 4, 128, 2, norm='instance')
-    x = dis_downsample(x, 4, 256, 2, norm='instance')
-    x = dis_downsample(x, 4, 512, 1, norm='instance')
-    x = tf.keras.layers.Conv2D(filters=1, kernel_size=4, strides=1, padding='same')(x)
+    x = dis_downsample(x, 4, 128, 2, norm='batch')
+    x = dis_downsample(x, 4, 256, 2, norm='batch')
+    x = dis_downsample(x, 4, 512, 1, norm='batch')
+    x = tf.keras.layers.Conv2D(filters=1, kernel_size=4, strides=1, padding='same', kernel_initializer=initializer)(x)
     return tf.keras.Model(inputs=inputs, outputs=x)
 
 def create_LSdiscriminator(shape=(256, 256, 3)):
