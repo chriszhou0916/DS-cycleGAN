dad898a7709512aec0deb35cc3e563a5bcf0752b
Leo Hu <LeoHuTwin@gmail.com>
2019-11-18 18:03:50 -0500

    Closes #5, closes #6

--- trainer/utils/dataset.py
+++ trainer/utils/dataset.py
@@ -1,12 +1,14 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds
+import sys
+sys.path.append('..')
+from trainer.config import config
+
 AUTOTUNE = tf.data.experimental.AUTOTUNE
-
-
-BUFFER_SIZE = 1000
-BATCH_SIZE = 1
-IMG_WIDTH = 256
-IMG_HEIGHT = 256
+BUFFER_SIZE = config.buffer_size
+BATCH_SIZE = config.bs
+IMG_WIDTH = config.in_w
+IMG_HEIGHT = config.in_h
 
 def random_crop(image):
   cropped_image = tf.image.random_crop(
@@ -38,13 +40,6 @@ def preprocess_image_test(image, label):
   image = normalize(image)
   return image
 def generate_dataset():
-    AUTOTUNE = tf.data.experimental.AUTOTUNE
-
-
-    BUFFER_SIZE = 1000
-    BATCH_SIZE = 1
-    IMG_WIDTH = 256
-    IMG_HEIGHT = 256
     dataset, metadata = tfds.load('cycle_gan/horse2zebra',
                                   with_info=True, as_supervised=True)
 
@@ -53,18 +48,18 @@ def generate_dataset():
 
     train_horses = train_horses.map(
         preprocess_image_train, num_parallel_calls=AUTOTUNE).shuffle(
-        BUFFER_SIZE).repeat().batch(1).prefetch(buffer_size=AUTOTUNE)
+        BUFFER_SIZE).batch(BATCH_SIZE).repeat().prefetch(buffer_size=AUTOTUNE)
 
     train_zebras = train_zebras.map(
         preprocess_image_train, num_parallel_calls=AUTOTUNE).shuffle(
-        BUFFER_SIZE).batch(1).repeat().prefetch(buffer_size=AUTOTUNE)
+        BUFFER_SIZE).batch(BATCH_SIZE).repeat().prefetch(buffer_size=AUTOTUNE)
 
     test_horses = test_horses.map(
         preprocess_image_test, num_parallel_calls=AUTOTUNE).shuffle(
-        BUFFER_SIZE).batch(1).repeat().prefetch(buffer_size=AUTOTUNE)
+        BUFFER_SIZE).batch(BATCH_SIZE).repeat().prefetch(buffer_size=AUTOTUNE)
 
     test_zebras = test_zebras.map(
         preprocess_image_test, num_parallel_calls=AUTOTUNE).shuffle(
-        BUFFER_SIZE).batch(1).repeat().prefetch(buffer_size=AUTOTUNE)
+        BUFFER_SIZE).batch(BATCH_SIZE).repeat().prefetch(buffer_size=AUTOTUNE)
 
     return train_horses, train_zebras, test_horses, test_zebras
